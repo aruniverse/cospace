e7884120dab6ecc4efeca3df52dcee16155640f4
Arun George <aruniverse@users.noreply.github.com>
2023-10-27 06:25:48 -0400

    able to send lockfile to explorer

--- cli/explore.ts
+++ cli/explore.ts
@@ -1,23 +1,32 @@
 import open from "open";
 import http from "node:http";
+import fs from "node:fs";
+import handler from "serve-handler";
+
+import { getWorkspaceRoot } from "workspace-tools";
+// import yaml from "yaml";
+import yaml from "js-yaml";
 
 export const explore = async () => {
   const PORT = 8765;
   const SERVICE_URL = `http://localhost:${PORT}`;
 
   const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === "/") {
-      // const html = renderClient({
-      //   title: "pnpm workspace graph",
-      //   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      //   viewerData: this._viewerData!,
-      //   enableWebSocket: true,
-      // });
-      res.writeHead(200, { "Content-Type": "text/html" });
-      // res.end(html);
-    } else {
-      // sirvMiddleware(req, res);
+    if (req.url === "/api/lockfile") {
+      console.log("attempt to fetch lockfile");
+      const cospaceRoot = getWorkspaceRoot(process.cwd(), "pnpm");
+      const lockfile = yaml.load(
+        fs.readFileSync(`${cospaceRoot}/pnpm-lock.yaml`, "utf8")
+      );
+
+      res
+        .writeHead(200, { "Content-Type": "application/json" })
+        .end(JSON.stringify(lockfile), "utf8");
     }
+
+    return handler(req, res, {
+      public: "../explorer-react/dist",
+    });
   });
 
   server.listen({ port: PORT }, async () => {
--- cli/index.ts
+++ cli/index.ts
@@ -6,17 +6,10 @@ import { fileURLToPath } from "node:url";
 import fs from "fs-extra";
 import { globby } from "globby";
 import meow from "meow";
-// import open from "open";
 
 // import { parseArgs } from "node:util";
+// parseArgs({})
 
-// parseArgs({
-//   options: {
-
-//   }
-// })
-
-// import http from "node:http";
 import { explore } from "./explore.js";
 
 // since __filename and __dirname are undefined for esm, define ourselves
@@ -218,28 +211,6 @@ const updateLockfile = async () => {
     }
   }
 };
-
-// const explore = async () => {
-//   const server = http.createServer((req, res) => {
-//     if (req.method === "GET" && req.url === "/") {
-//       // const html = renderClient({
-//       //   title: "pnpm workspace graph",
-//       //   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-//       //   viewerData: this._viewerData!,
-//       //   enableWebSocket: true,
-//       // });
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       // res.end(html);
-//     } else {
-//       // sirvMiddleware(req, res);
-//     }
-//   });
-//   server.listen({ port: 8765 }, () => {
-//     console.log("Listening on http://localhost:8765");
-//   });
-
-//   await open("http://localhost:8765");
-// };
 
 const run = async () => {
   const { input, flags, showHelp, showVersion } = meow(help, {
